259176ca49262a19fea606120801a2e5e85731a8
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-05-16 14:09:02 +0900

    set font

--- components/TopTitle.tsx
+++ components/TopTitle.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 const TopTitle = (props: Props) => {
   return (
-    <h1 className="my-32 text-center text-7xl">
+    <h1 className="my-32 text-center text-6xl font-medium">
       { props.title }
     </h1>
   )
