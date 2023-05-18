916e2ec622ee517e5ab12004a19ae335001b5565
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-05-18 18:05:23 +0900

    add text

--- components/AboutContent.tsx
+++ components/AboutContent.tsx
@@ -3,24 +3,26 @@ type rankType = {
 	content: string
 }
 type Props = {
-	title: string
-	src: string
-	rank: rankType[]
+	content: {
+		title: string
+		src: string
+		rank: rankType[]
+	}
 }
 
 const AboutContent = (props: Props) => {
-	const ranks = props.rank
+	const ranks = props.content.rank
 	return (
 		<>
 			<div className='col-md-4'>
 				<div className='row justify-content-center content-title'>
 					<div className='col-12'>
-						<h3>{props.title}</h3>
+						<h3>{props.content.title}</h3>
 					</div>
 				</div>
 				<div className='row justify-content-center'>
 					<div className='col-lg-4 col-md-8 col-6'>
-						<img src={props.src} alt={props.title + 'image'} />
+						<img src={props.content.src} alt={props.content.title + 'image'} className='w-16 h-16' />
 					</div>
 				</div>
 			</div>
