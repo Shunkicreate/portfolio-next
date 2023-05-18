926371f86c175e0c12444f24e7388735e12a6246
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-05-18 18:21:47 +0900

    make server side

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,6 @@
+import { promises as fs } from 'fs'
+import path from 'path'
+import { GetServerSideProps } from 'next'
 import AboutContent from '../components/AboutContent'
 
 type rankType = {
@@ -10,111 +13,32 @@ type Content = {
 	rank: rankType[]
 }
 
-const About = () => {
-	const contents: Content[] = [
-		{
-			title: 'Games',
-			src: '/icons/game.svg',
-			rank: [
-				{
-					rank: 1,
-					content: 'スーパーマリオ',
-				},
-				{
-					rank: 2,
-					content: 'Genshin Impact',
-				},
-				{
-					rank: 3,
-					content: 'プロジェクトセカイ',
-				},
-			],
-		},
-		{
-			title: 'Music',
-			src: '/icons/music.svg',
-			rank: [
-				{
-					rank: 1,
-					content: 'Mr.Children',
-				},
-				{
-					rank: 2,
-					content: 'Vocaloid(Deco*27, wowaka)',
-				},
-				{
-					rank: 3,
-					content: 'Radwimps',
-				},
-			],
-		},
-		{
-			title: 'Books',
-			src: '/icons/books.svg',
-			rank: [
-				{
-					rank: 1,
-					content: '告白 湊かなえ',
-				},
-				{
-					rank: 2,
-					content: '砂漠 伊坂幸太郎',
-				},
-				{
-					rank: 3,
-					content: '蜜蜂と遠雷 奥田陸',
-				},
-			],
-		},
-		{
-			title: 'PC',
-			src: '/icons/pc.svg',
-			rank: [
-				{
-					rank: 1,
-					content: 'CPU: AMD Ryzen5 3600X',
-				},
-				{
-					rank: 2,
-					content: 'GPU: NVIDIA GeForce GTX1650',
-				},
-				{
-					rank: 3,
-					content: 'Memory: T-FORCE VULCAN Z DDR4-3200 8GB',
-				},
-			],
-		},
-		{
-			title: 'Coding Lang',
-			src: '/icons/code.svg',
-			rank: [
-				{
-					rank: 1,
-					content: 'Python',
-				},
-				{
-					rank: 2,
-					content: 'C',
-				},
-				{
-					rank: 3,
-					content: 'Next.js, React, Vue.js',
-				},
-				{
-					rank: 4,
-					content: 'Processing',
-				},
-				{
-					rank: 5,
-					content: 'GCP, Azure, AWS',
-				},
-			],
-		},
-	]
+type Props = {
+	contents: Content[]
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const ContentDirectory = path.join(process.cwd(), 'aboutdata.json')
+	type ContentDataType = {
+		data: Content[]
+	}
+	const aboutContentsStringData = await fs.readFile(ContentDirectory, 'utf8')
+	const aboutContents = JSON.parse(aboutContentsStringData) as ContentDataType
+	const PropsAboutContents = aboutContents.data
+	const props: Props = {
+		contents: PropsAboutContents,
+	}
+
+	return {
+		props: props,
+	}
+}
+
+const About = (props: Props) => {
 	return (
 		<>
 			<div>about</div>
-			{contents.map((content, i) => {
+			{props.contents.map((content, i) => {
 				return <AboutContent key={i} content={content} />
 			})}
 		</>
