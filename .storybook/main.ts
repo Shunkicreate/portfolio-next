c9148120c0172d9005131333088e59b8cff87253
shunki-tada <shunki.tada@dena.jp>
2025-06-07 13:23:22 +0900

    del vitest

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,40 +1,16 @@
 // .storybook/main.ts
-import type { StorybookConfig } from '@storybook/nextjs-vite'
-  import { mergeConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
+import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
 	stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: [
-		'@storybook/addon-onboarding',
-		'@chromatic-com/storybook',
-		'@storybook/addon-docs',
-		'@storybook/addon-a11y',
-		'@storybook/addon-vitest',
-	],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
 	framework: {
-		name: '@storybook/nextjs-vite',
+		name: '@storybook/nextjs',
 		options: {},
 	},
-	staticDirs: ['../public'],
-
-	// ここから追加 ↓
-	async viteFinal(viteConfig) {
-		return mergeConfig(viteConfig, {
-			plugins: [
-				react({
-					babel: {
-						plugins: ['styled-jsx/babel'],
-					},
-				}),
-			],
-			optimizeDeps: {
-				include: ['styled-jsx'],
-			},
-		})
+	docs: {
+		autodocs: 'tag',
 	},
-	// ここまで追加 ↑
 }
 
 export default config
-
